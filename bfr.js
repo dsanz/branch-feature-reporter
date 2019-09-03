a1f152539f2d87b1dfa3097d68ca8b672490a22e
Daniel Sanz <daniel.sanz@liferay.com>
2019-09-03 10:17:22 +0200

    Initial version: runs jira query, checks in branch, creates json and csv reports in the stdout

---
+++ bfr.js
@@ -0,0 +1,294 @@
+const JiraApi = require('jira-client');
+const util = require('util');
+const process = require('process');
+const exec = require('child_process').execSync;
+const PropertiesReader = require('properties-reader');
+//const mod = require('./mod');
+
+const jiraProps = PropertiesReader('jira.properties');
+
+// Initialize
+var jira = new JiraApi({
+	protocol: 'https',
+	host: 'issues.liferay.com',
+	username: jiraProps.get('jira.username'),
+	password: jiraProps.get('jira.password'),
+	apiVersion: '2',
+	strictSSL: true
+});
+
+
+var epics = new Object(); // stories grouped by epic
+var stories = new Object() ; // stories w/o associated epic
+var tasks = new Object(); // tech tasks not associated to stories nor epics
+var resultCache = []; // all issues as returned by JIRA REST API
+
+/* get some data from json objects returned by JIRA REST API */
+function buildIssueKey(issue) {	return issue.key + ': ' + issue.fields.summary; }
+function getType(issue) { return issue.fields.issuetype.name;}
+function getStatus(issue) { return issue.fields.status.name; }
+function getEpicLink(issue) { return issue.fields.customfield_12821; }
+function getSummary(issue) { return issue.fields.summary; }
+function isEpic(issue) { return "Epic" == getType(issue) }
+function isStory(issue) { return "Story" == getType(issue)	}
+function isTask(issue) {
+	return ("Task" == getType(issue)) || ("Technical Task" == getType(issue));
+}
+
+function sanitize(text) {
+	return text.replace(/\t/g," ");
+}
+
+// convert a JIRA issue to the JSON representation we want for output
+function convert(issue) {
+	//key = getType(issue) + " (" + issue.fields.status.name + ") ";
+	return { "fields": { "summary": getSummary(issue),
+			"status": getStatus(issue),
+			"type": getType(issue) }}
+}
+
+function cacheIssue(issue) {
+	resultCache[issue.key] = issue
+}
+
+async function findAndCacheIssue(issueKey) {
+	const issue = await jira.findIssue(issueKey);
+	cacheIssue(issue);
+	return issue;
+}
+
+async function getParentIssue(issue) {
+	if (issue.fields.parent) {
+		if (!resultCache[issue.fields.parent.key]) { // parent may have been filtered out by the jira query
+			await findAndCacheIssue(issue.fields.parent.key)
+		}
+		return resultCache[issue.fields.parent.key]
+	}
+	else return null;
+}
+
+// issue comes as the epic key as we add the epic via addStory so we just have the epic link field
+async function addEpic(epicLink) { 
+	//console.log("Adding epic " + issue)
+	try {
+		if (!epics[epicLink]) {
+			epics[epicLink] = new Object();	// let add a placeholder just in case
+			// epic may have been filtered out by the jira query
+			const epic = await findAndCacheIssue(epicLink)
+			epics[epicLink] = convert(epic)
+		}
+	}
+	catch (err) {
+		console.error(err)
+	}
+	return epics[epicLink];
+}
+
+async function addStory(issue) {
+	epicLink = getEpicLink(issue)
+	if (epicLink) {
+		//console.log("  story " + issue.key + " has epic")	
+		epic = await addEpic(epicLink)
+		if (!epic[issue.key]) {
+			epic[issue.key] = convert(issue)
+		}
+		return epic[issue.key]
+	}
+	else {
+		if (!stories[issue.key]) {
+			stories[issue.key] = convert(issue)
+		}
+		return stories[issue.key]
+	}
+}
+
+async function addTask(issue) {
+	//console.log("Adding task " + issue.key)	
+	parent = await getParentIssue(issue)
+	if (parent) {
+		//console.log("   task " + issue.key + " has parent")	
+		if (isStory(parent)) {
+			story = await addStory(parent)
+			if (!story[issue.key]) {
+				story[issue.key] = convert(issue);
+			}
+			return story[issue.key]
+		}
+		else if (isTask(parent)) {
+			task = await addTask(parent);
+			if (!task[issue.key]) {
+				task[issue.key] = convert(issue)
+			}
+			return task[issue.key]
+		}
+		else {
+			console.log(issue.key + " has a parent which is neither a story nor a task")
+			return null;
+		}
+	}
+	else {
+		epicLink = getEpicLink(issue)
+		if (epicLink) {
+			epic = await addEpic(epicLink)
+			if (!epic[issue.key]) {
+				epic[issue.key] = convert(issue)
+			}
+			return epic[issue.key]
+		}
+		else {
+			if (!tasks[issue.key]) {
+				tasks[issue.key] = convert(issue)
+			}
+			return tasks[issue.key]
+		}
+	}
+}
+
+async function addIssue(issue) {
+	try {
+		if (isStory(issue)) {
+			await addStory(issue)
+		}
+		else if (isTask(issue)) {
+			await addTask(issue)
+		}
+		else {
+			console.log("Issue " + issue.key + " is neither a task or story, it should not have committed code");
+		}
+	}
+	catch (err) {
+		console.log(err)
+	}
+}
+
+function isTicketinHistory(issue) {
+	command = "git log --oneline " + jiraProps.get('branch.ref.from') + ".." +
+			  jiraProps.get('branch.ref.to') + " | grep " + issue.key;
+	try {
+		stdout = exec(command);
+		return (stdout.length != 0);
+	}
+	catch(err) {
+	}
+	return false;
+}
+
+function logCSVLine(csvLine) {
+	console.log(Object.keys(csvLine).reduce( (total, k, i, a) => {
+		return total + csvLine[k] + ((i == a.length -1 ) ? "" : "\t");
+	}, ""));
+}
+
+function buildCSVLine(issueKey, issue, epicRendered, epicKey, epic) {
+	epicLine = "none"
+	if (epicKey && epic) {
+		epicLine = epicRendered ? "" : (epicKey + ": " + sanitize(getSummary(epic)));
+	}
+	return {
+		"epic" : epicLine,
+		"feature" : "[" + getType(issue) +"] → " + sanitize(getSummary(issue)) + "",
+		"LPS" : issueKey,
+		"status": getStatus(issue),
+		"subtasks" : Object.keys(issue).reduce( (total, k, i, a) => {
+			if (k == "fields") {
+				return total;
+			}
+			return total + k + "(" + getStatus(issue[k]) + ")" + ((i == a.length -1) ? "" : ":");
+		}, "")
+	};
+}
+
+
+function printCSV(data) {
+	epics = data["EPICS"]
+
+	console.log("Epic\tElement/Feaure\tLPS\tStatus\tSubtasks");
+	for (epicKey in epics) {
+		epic = epics[epicKey];
+		epicRendered = false;
+		for (issueKey in epic) {
+			if (issueKey == "fields") { continue; }
+			issue = epic[issueKey];
+			logCSVLine(buildCSVLine(issueKey, issue, epicRendered, epicKey, epic));
+			epicRendered = true;
+		}
+	}
+
+	stories = data["STORIES W/O EPIC"];
+	for (storyKey in stories) {
+		logCSVLine(buildCSVLine(storyKey, stories[storyKey]));
+	}
+
+	tasks = data["TASKS W/O STORY"];
+	for (taskKey in tasks) {
+		logCSVLine(buildCSVLine(taskKey, tasks[taskKey]));
+	}
+}
+
+function printJSON(data) {
+	console.log(util.inspect(data, {showHidden: false, depth:null, colors:true, sorted:true, compact:false, breakLength:Infinity}));
+}
+
+async function getTickets() {
+	try {
+		console.log("Querying JIRA: " + jiraProps.get('jira.query'));
+		const issues = await jira.searchJira(jiraProps.get('jira.query'), {maxResults:500})
+
+		console.log("Caching " + issues.issues.length + " issues")
+		for (let index = 0; index < issues.issues.length; index++) {
+			cacheIssue(issues.issues[index])
+		}
+
+		for (const branch of ["public","private"]) {
+			console.log("Processing " + jiraProps.get('branch.name.' + branch) + "@"+ jiraProps.get('branch.dir.' + branch));
+
+			process.chdir(jiraProps.get('branch.dir.'+branch));
+			if (jiraProps.get('branch.sync')) {
+				console.log("Checking out " + jiraProps.get('branch.name.'+branch))
+				await exec("git checkout " + jiraProps.get('branch.name.'+branch))
+				console.log("Pulling " + jiraProps.get('branch.name.'+branch) + " from upstream")
+				await exec("git pull upstream " + jiraProps.get('branch.name.'+branch))
+			}
+
+			console.log("Building feature tree from git history");
+			issueCount = 0;
+			for (let index = 0; index < issues.issues.length; index++) {
+				char = '-'
+				if (isTicketinHistory(issues.issues[index])) {
+					char="·" //
+					issueCount++;
+					await addIssue(issues.issues[index])
+				}
+				process.stdout.write(char)
+			}
+			console.log()
+			console.log(issueCount + " out of " + issues.issues.length + " issues were found in git")
+		}
+
+		var all = new Object();
+		all["EPICS"]=epics;
+		all["STORIES W/O EPIC"]=stories;
+		all["TASKS W/O STORY"]=tasks;
+		process.chdir(process.env.PWD)
+		return all;
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
+
+async function run() {
+	try {
+		var tickets = await getTickets();
+		printJSON(tickets)
+		printCSV(tickets)
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
+
+//mod.testmod()
+
+run();
+
---
+++ readme.md
@@ -0,0 +1,49 @@
+#Branch Feature Reporter
+
+This script reports features for which there are commits in the given commit range and which match with a JIRA filter. 
+It works as follows:
+* JIRA query is run
+* Branch is inspected to find traces of each ticket returned by the query
+* If ticket is found, it's added to the report
+* Features are hierarchically grouped by Epic/Story/Task levels using JIRA information.
+
+Report is given in JSON and CSV formats. A few, essential information is added to each ticket such as summary, type and status.
+
+##Usage
+### Configuration
+Tool reads jira.properties file in the directory from where it's invoked. 
+List of properties that must be present:
+
+```
+ ###############################
+ # jira
+ 
+ # server connection 
+ jira.username=user.name
+ jira.password=****
+ 
+ # queries returning tickets we want to check for existence in the source history
+ jira.query=filter = "Components | LPS-Lima" and status changed after "2019/05/31" and issuetype in (Task, "Technical Task", Story)
+ 
+ ###############################
+ # source code
+ 
+ # where is the code?
+ branch.dir.public=/home/dsanz/projects/72x/7.2.x
+ branch.dir.private=/home/dsanz/projects/72x/7.2.x-private
+ 
+ # do we want to sync the branch before running? which one?
+ branch.sync=true
+ branch.name.public=7.2.x
+ branch.name.private=7.2.x-private
+ 
+ # commit range to look for existence of tickets returned by query
+ branch.ref.from=7.2.0-ga1
+ branch.ref.to=HEAD` 
+```
+### Command-line invocation
+
+### Report contents
+
+# Features to come
+Profiles to store many queries in this file. Output files saved by profile. Date/time stamps on output
